8017c5bb2a915d42db73bdbc4afd6344992c4fa1
Thomas Mercer <tmercer74@gmail.com>
2017-07-24 16:16:04 -0700

    updated js files

--- keys.js
+++ keys.js
@@ -1,51 +1,51 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
-
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-
-
-
-var Twitter = require('twitter');
- 
-var client = new Twitter({
   consumer_key: '8XLxMZ84rjkiuCWsZisTtCg5z',
   consumer_secret: '2xe9ZGM01ysVwx1M3r7m3qbEXnF1pkRsNlb7f0O7PUFQCW2LgI',
   access_token_key: '877026527891685377-PcZ1X63zWUeQKANcJ2eqE7SsHtZF8Mm',
   access_token_secret: 'wVVNQBRQdUytrOqPIPQjJKcfVlScAhaSj4XTulkslOGyf'
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+}
+
+// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 
 
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
-});
- 
-spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
 
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred 
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-  console.log('body:', body); // Print the HTML for the Google homepage. 
-});
+// var Twitter = require('twitter');
+ 
+// var client = new Twitter({
+//   consumer_key: '8XLxMZ84rjkiuCWsZisTtCg5z',
+//   consumer_secret: '2xe9ZGM01ysVwx1M3r7m3qbEXnF1pkRsNlb7f0O7PUFQCW2LgI',
+//   access_token_key: '877026527891685377-PcZ1X63zWUeQKANcJ2eqE7SsHtZF8Mm',
+//   access_token_secret: 'wVVNQBRQdUytrOqPIPQjJKcfVlScAhaSj4XTulkslOGyf'
+// });
+ 
+// var params = {screen_name: 'mortontobias'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
+
+
+// var Spotify = require('node-spotify-api');
+ 
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+ 
+// spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
+
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred 
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+//   console.log('body:', body); // Print the HTML for the Google homepage. 
+// });
--- liri.js
+++ liri.js
@@ -1,82 +1,12 @@
-"use strict";
-(function() {
-	var user = require("./user");
-	var userArg = process.argv[2];
-	var Auser = user(config);
-})();
-// var list
-var getTweets = function() {
-	var client = new twitter(dataKeys.twitterKeys);
-}
-//variables for these functions
-var newVariable = results;
-
-
-var request = require('request');
-var spotify = require('spotify');
-var fs = require('fs');
 var keys = require('./keys.js');
-var querystring = require('querystring');
-var random = require('./random.js');
 
-var command = process.argv[2];
-var argument = process.argv[3];
-var output;
+var Twitter = require('./twitter');
 
-//spotify access
-$.ajax(
-  {
-    method: "POST",
-    url: "https://accounts.spotify.com/api/token",
-    data: {
-      "grant_type":    "authorization_code",
-      "code":          code,
-      "redirect_uri":  myurl,
-      "client_secret": mysecret,
-      "client_id":     myid,
-    },
-    success: function(result) {
-      // handle result...
-    },
-  }
-);
+var client = new Twitter(keys.twitterKeys);
 
-//sets the default 
-function spotifyThisSong() {
-	if(argument == null) {
-		argument = "The Sign Ace of Base";
-
+var params = {screen_name: 'tobias_morton'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	if (!error) {
+		console.log(tweets);
 	}
-	spotify.search({type: 'track', query: argument}, function(error, data) {
-		var thisSong;
-			if(!error && (data.tracks.items.length >= 1)) {
-				thisSong = data.tracks.items[0];
-				var artistConcat = thisSong.artists[0].name;
-				for(var a = 1; a < thisSong.artists.length; a++) {
-					artistConcat += ', ' + thisSong.artists[a].name;
-				}
-				output = ('\nSong Info \n\nArtist: ' + artistConcat + '\n\nSong Title: ' + thisSong.name + '\n\nOriginal Album: ' + thisSong.album.name + '\n\nPreview: ' + thisSong.preview_url + '\n');
-				console.log(output);
-				append();
-			
-			} else {
-				console.log('spotify error or there is no song matching that title.');
-			}
-		});
-}
-//sets the movie search..and default movie for incorrect searhc
-function movieThis() {
-	if (argument == null) {
-		argument = "Mr. Nobody";
-	}
-	var queryProps = {
-		t: argument,
-		plot: 'full',
-		r: 'json',
-		tomatoes: true
-	}
-	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-
-
-}
-
+});
